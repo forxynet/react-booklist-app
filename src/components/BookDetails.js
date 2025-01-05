56d1345e6b2f0d04846786dfa9f643de40a0912e
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-05 16:52:03 +0300

    added book form

---
+++ src/components/BookDetails.js
@@ -0,0 +1,12 @@
+import { useContext } from 'react'
+import { BookContext } from '../contexts/BookContext'
+export default function BookDetails({ book }) {
+  const { removeBook } = useContext(BookContext);
+
+  return (
+    <li onClick={() => removeBook(book.id)}>
+      <div className='title'>{book.title}</div>
+      <div className='auther'>{book.author}</div>
+    </li>
+  )
+}
---
+++ src/components/BookForm.js
@@ -0,0 +1,38 @@
+import { useContext, useState } from 'react'
+import { BookContext } from '../contexts/BookContext'
+
+export default function NewBookForm() {
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    clearForm();
+  }
+
+  function clearForm() {
+    setTitle('');
+    setAuthor('');
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='book title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type='text'
+        placeholder='book author'
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+        required
+      />
+      <input type='submit' value='add book' />
+    </form>
+  )
+}
